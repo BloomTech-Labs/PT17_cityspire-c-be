d1ad980da94d68b7e5ca263a3d89eda64c9fe9e0
abhitsahota <abhit.sahota@gmail.com>
2021-04-14 21:23:31 -0400

    editted some formatting mistakes and missed brackets

--- data/migrations/20210414210812_add-favs.js
+++ data/migrations/20210414210812_add-favs.js
@@ -1,12 +1,13 @@
 // Adds a column for favourites attached to the profile table
 
 exports.up = function (knex) {
-  return knex.schema.table('profiles') function (table){
-      table.json('favs')
-  }
+  return knex.schema.table('profiles', function (table) {
+    table.json('favs');
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema.table('profiles') function (table){
-        table.dropColumn('favs')
+  return knex.schema.table('profiles', function (table) {
+    table.dropColumn('favs');
+  });
 };
